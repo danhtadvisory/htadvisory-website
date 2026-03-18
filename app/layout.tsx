448580cd38882f9fba83896298e6f411aa34aeb3
Dan <daniel@htadvisory.co.nz>
2026-03-18 18:22:08 +1300

    Fix macron font

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;700;900&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
